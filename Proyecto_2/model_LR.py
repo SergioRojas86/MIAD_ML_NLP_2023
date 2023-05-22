e5eb7199c01f63f5ba49760f51e3c888e2d029f3
SergioRojas86 <rojas.sergio86@hotmail.com>
2023-05-22 18:22:40 -0500

    adding new files

--- Proyecto_2/model_LR.py
+++ Proyecto_2/model_LR.py
@@ -12,6 +12,7 @@ def predict_genre(features):
     
     regressor = joblib.load(os.path.dirname(__file__) + '/movie_genres_LR.pkl')
     le = MultiLabelBinarizer()
+    tfidf_vectorizer = TfidfVectorizer()
     
     def clean_text(text):
         text = re.sub((r'[^\w\s]'),'', text).lower() 
