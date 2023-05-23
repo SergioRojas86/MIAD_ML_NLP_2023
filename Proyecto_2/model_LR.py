d84c55cd8514dcc7bb26266044b09ac31a3a4184
SergioRojas86 <rojas.sergio86@hotmail.com>
2023-05-22 19:36:39 -0500

    adding new files

--- Proyecto_2/model_LR.py
+++ Proyecto_2/model_LR.py
@@ -11,6 +11,8 @@ from sklearn.preprocessing import MultiLabelBinarizer
 def predict_genre(features):
     
     regressor = joblib.load(os.path.dirname(__file__) + '/movie_genres_LR.pkl')
+    vectorizer =joblib.load(os.path.dirname(__file__) + 'trained/vectorizer.pkl')
+    
     #le = MultiLabelBinarizer()
     tfidf_vectorizer = TfidfVectorizer()
     
@@ -23,23 +25,12 @@ def predict_genre(features):
         words = re.sub(r'[^\w\s]', ' ', text).split()
         return ' '.join([wnl.lemmatize(word) for word in words if word not in stopwords])
     
-    ####################################################################################################################
-    
-    # Carga de datos de archivo .csv
-    #dataTraining = pd.read_csv('https://github.com/albahnsen/MIAD_ML_and_NLP/raw/main/datasets/dataTraining.zip', encoding='UTF-8', index_col=0)
-    #dataTraining['clean_plot'] = dataTraining['plot'].apply(clean_text)
-    
-    #xtrain_tfidf = tfidf_vectorizer.fit_transform(dataTraining['clean_plot'])
-    #xtrain_tfidf = tfidf_vectorizer.transform(dataTraining['clean_plot'])
-    
-    ###################################################################################################################
-    
     df5000 = pd.DataFrame(columns=['plot'],index=range(1))
     df5000['plot'] = features
     df5000['clean_plot'] = df5000['plot'].apply(clean_text)
      
     q = df5000['clean_plot']
-    q_vec = tfidf_vectorizer.transform(q)
+    q_vec = vectorizer.transform(q)
     
     # Make prediction
     q_pred = regressor.predict(q_vec)
