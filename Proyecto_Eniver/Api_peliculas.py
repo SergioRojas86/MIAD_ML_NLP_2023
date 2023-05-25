8d7f528d423aabdfd3d37022cf92ef99235d728b
SergioRojas86 <rojas.sergio86@hotmail.com>
2023-05-25 10:53:55 -0500

    uploading files

--- Proyecto_Eniver/Api_peliculas.py
+++ Proyecto_Eniver/Api_peliculas.py
@@ -37,7 +37,7 @@ class MovieApi(Resource):
     def get(self):
         args = parser.parse_args()
         return {
-         "result": prediccion_genero([args['Plot']])
+         "result": predict_genre([args['Plot']])
         }, 200
     
     
